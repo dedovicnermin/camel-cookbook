0571868f46122a704a5d59d91094ec769ccb86d7
Nermin Dedovic <dedovicnermin@outlook.com>
2021-07-26 11:13:10 -0500

    removing unused imports

--- rest-dsl/src/test/java/tech/nermindedovic/restdsl/OrderProcessorTest.java
+++ rest-dsl/src/test/java/tech/nermindedovic/restdsl/OrderProcessorTest.java
@@ -5,15 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import tech.nermindedovic.restdsl.dto.Order;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
