003a2fd2e0c3efb6999f94cfea1bca7788f25777
Nermin Dedovic <dedovicnermin@outlook.com>
2021-07-28 09:38:16 -0500

    Setting up multicast-demo module

---
+++ multicast-demo/src/main/java/tech/nermindedovic/multicastdemo/MulticastDemoApplication.java
@@ -0,0 +1,13 @@
+package tech.nermindedovic.multicastdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulticastDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MulticastDemoApplication.class, args);
+    }
+
+}
---
+++ multicast-demo/src/test/java/tech/nermindedovic/multicastdemo/MulticastDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nermindedovic.multicastdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulticastDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
