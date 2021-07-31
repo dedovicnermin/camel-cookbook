006452c0d53bb892504b9dda678d55aade945184
Nermin Dedovic <dedovicnermin@outlook.com>
2021-07-31 18:54:14 -0500

    recipient-list-demo
    initial commit

---
+++ recipient-list-demo/src/main/java/tech/nermindedovic/recipientlistdemo/RecipientListDemoApplication.java
@@ -0,0 +1,13 @@
+package tech.nermindedovic.recipientlistdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipientListDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipientListDemoApplication.class, args);
+    }
+
+}
