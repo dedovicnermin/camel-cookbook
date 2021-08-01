e5b5e123aabbdfc37bbf1734bcc8e6dfc8151396
Nermin Dedovic <dedovicnermin@outlook.com>
2021-08-01 16:32:30 -0500

    one-way req/resp routing demo
    initial commmit

---
+++ reqresp-routing-demo/src/main/java/tech/nermindedovic/reqresproutingdemo/ReqrespRoutingDemoApplication.java
@@ -0,0 +1,13 @@
+package tech.nermindedovic.reqresproutingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReqrespRoutingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReqrespRoutingDemoApplication.class, args);
+    }
+
+}
---
+++ reqresp-routing-demo/src/test/java/tech/nermindedovic/reqresproutingdemo/ReqrespRoutingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nermindedovic.reqresproutingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReqrespRoutingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
