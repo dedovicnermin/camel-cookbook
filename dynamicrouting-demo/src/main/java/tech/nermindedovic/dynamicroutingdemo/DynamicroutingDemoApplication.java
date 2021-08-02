c1b714f4e239532e106b4beef790d4d929157800
Nermin Dedovic <dedovicnermin@outlook.com>
2021-08-01 19:19:20 -0500

    dynamic routing EIP
    implemented + tested the Dynamic Router EIP. Where messages will continue to route to dynamically set destinations until router expression returns null.

---
+++ dynamicrouting-demo/src/main/java/tech/nermindedovic/dynamicroutingdemo/DynamicroutingDemoApplication.java
@@ -0,0 +1,13 @@
+package tech.nermindedovic.dynamicroutingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicroutingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicroutingDemoApplication.class, args);
+    }
+
+}
